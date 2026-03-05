06f4f2464b55240a06a53cde1777e715060ce27d
lionel sisso <sissolionel@gmail.com>
2026-03-05 11:04:07 +0100

    fixing pagination

--- resources/views/pagination/bootstrap-5-sm.blade.php
+++ resources/views/pagination/bootstrap-5-sm.blade.php
@@ -1,66 +1,76 @@
 @if ($paginator->hasPages())
-    <nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="d-flex justify-content-between align-items-center">
-        <div class="d-flex flex-column flex-sm-row justify-content-between flex-wrap align-items-center w-100">
-            <div class="mb-2 mb-sm-0">
-                <p class="text-sm text-muted mb-0">
-                    {!! __('Showing') !!}
-                    @if ($paginator->firstItem())
-                        <span class="fw-semibold">{{ $paginator->firstItem() }}</span>
-                        {!! __('to') !!}
-                        <span class="fw-semibold">{{ $paginator->lastItem() }}</span>
-                    @else
-                        {{ $paginator->count() }}
-                    @endif
-                    {!! __('of') !!}
-                    <span class="fw-semibold">{{ $paginator->total() }}</span>
-                    {!! __('results') !!}
-                </p>
-            </div>
+<nav role="navigation" aria-label="{{ __('Pagination Navigation') }}" class="cuni-pagination-wrapper">
+    <div class="cuni-pagination-info">
+        <span class="cuni-pagination-text">
+            {!! __('Affichage de') !!}
+            @if ($paginator->firstItem())
+                <strong>{{ $paginator->firstItem() }}</strong>
+                {!! __('à') !!}
+                <strong>{{ $paginator->lastItem() }}</strong>
+            @else
+                {{ $paginator->count() }}
+            @endif
+            {!! __('sur') !!}
+            <strong>{{ $paginator->total() }}</strong>
+            {!! __('résultats') !!}
+        </span>
+    </div>
+    
+    <ul class="cuni-pagination">
+        {{-- Previous Page Link --}}
+        @if ($paginator->onFirstPage())
+            <li class="cuni-page-item disabled" aria-disabled="true">
+                <span class="cuni-page-link">
+                    <i class="bi bi-chevron-left"></i>
+                </span>
+            </li>
+        @else
+            <li class="cuni-page-item">
+                <a class="cuni-page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">
+                    <i class="bi bi-chevron-left"></i>
+                </a>
+            </li>
+        @endif
 
-            <div>
-                <ul class="pagination pagination-sm mb-0">
-                    {{-- Previous Page Link --}}
-                    @if ($paginator->onFirstPage())
-                        <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
-                            <span class="page-link" aria-hidden="true">&lsaquo;</span>
+        {{-- Pagination Elements --}}
+        @foreach ($elements as $element)
+            {{-- "Three Dots" Separator --}}
+            @if (is_string($element))
+                <li class="cuni-page-item disabled" aria-disabled="true">
+                    <span class="cuni-page-link cuni-page-dots">{{ $element }}</span>
+                </li>
+            @endif
+
+            {{-- Array Of Links --}}
+            @if (is_array($element))
+                @foreach ($element as $page => $url)
+                    @if ($page == $paginator->currentPage())
+                        <li class="cuni-page-item active" aria-current="page">
+                            <span class="cuni-page-link">{{ $page }}</span>
                         </li>
                     @else
-                        <li class="page-item">
-                            <a class="page-link" href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</a>
+                        <li class="cuni-page-item">
+                            <a class="cuni-page-link" href="{{ $url }}">{{ $page }}</a>
                         </li>
                     @endif
+                @endforeach
+            @endif
+        @endforeach
 
-                    {{-- Pagination Elements --}}
-                    @foreach ($elements as $element)
-                        {{-- "Three Dots" Separator --}}
-                        @if (is_string($element))
-                            <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
-                        @endif
-
-                        {{-- Array Of Links --}}
-                        @if (is_array($element))
-                            @foreach ($element as $page => $url)
-                                @if ($page == $paginator->currentPage())
-                                    <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
-                                @else
-                                    <li class="page-item"><a class="page-link" href="{{ $url }}">{{ $page }}</a></li>
-                                @endif
-                            @endforeach
-                        @endif
-                    @endforeach
-
-                    {{-- Next Page Link --}}
-                    @if ($paginator->hasMorePages())
-                        <li class="page-item">
-                            <a class="page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</a>
-                        </li>
-                    @else
-                        <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
-                            <span class="page-link" aria-hidden="true">&rsaquo;</span>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
+        {{-- Next Page Link --}}
+        @if ($paginator->hasMorePages())
+            <li class="cuni-page-item">
+                <a class="cuni-page-link" href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="@lang('pagination.next')">
+                    <i class="bi bi-chevron-right"></i>
+                </a>
+            </li>
+        @else
+            <li class="cuni-page-item disabled" aria-disabled="true">
+                <span class="cuni-page-link">
+                    <i class="bi bi-chevron-right"></i>
+                </span>
+            </li>
+        @endif
+    </ul>
+</nav>
 @endif
